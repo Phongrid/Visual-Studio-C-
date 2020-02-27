83ff66efe6d78db95d8d4839c445eed637418b68
Phongrid <Phongrid.y@hotmail.com>
2020-02-27 15:49:27 +0700

    add SQLite

---
+++ SQLiteApp/SQLiteApp/DataAccess .cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteApp
+{
+
+    class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {   //create connection to sqlite database file
+            using (SqliteConnection db = new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+            //oepn database and creat Table name= Primary_key(integer)PK // Text_Entry(NVARCHAR 2048)
+                db.Open();
+                String tableCommand = "CREATE TABLE IF NOT " +
+                                      "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                                      "Text_Entry NVARCHAR(2048) NULL)";
+            // connect SQLiteDatabase with command (SQLkey, dabaseConnect)
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+            // run  
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddData(string inputText)
+        {   //create connection to sqlite datanase file
+            using (SqliteConnection db = new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+                // run sqlcommand
+                insertCommand.ExecuteReader();
+                db.Close();
+            }
+        }
+
+        public static List<String> GetData()
+        {   // create "entries" as string for get result from SQLite database
+            List<String> entries = new List<string>();
+            // declaration object
+            using (SqliteConnection db = new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                // use SQL Command
+                SqliteCommand selectCommand = new SqliteCommand("SELECT Text_Entry from MyTable", db);
+                // create "SqliteDataReader" to get data in case database return data to use someting
+                SqliteDataReader query = selectCommand.ExecuteReader();
+                //create while loop 
+                // Read() -> return true if data remain in row
+                //           return false if not have data
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                }
+                db.Close();
+            }
+            return entries;
+        }
+    }
+}
---
+++ SQLiteApp/SQLiteApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLiteApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+         //  DataAccess.AddData("Phongrid Yoosaeng");
+        }
+
+        // add data
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(txt_input_value.Text);
+            txt_input_value.Clear();
+        } 
+
+        //show data
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Show
+            string output = string.Empty;
+            foreach (string txt_value in DataAccess.GetData())
+            {
+                output += txt_value + "\n";
+            }
+            MessageBox.Show(output);
+        }
+    }
+}
