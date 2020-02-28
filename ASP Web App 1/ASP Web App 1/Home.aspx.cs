79d4e4589a623515165244e4214f89613b6f0674
Phongrid <Phongrid.y@hotmail.com>
2020-02-28 15:20:37 +0700

    add

--- ASP Web App 1/ASP Web App 1/Home.aspx.cs
+++ ASP Web App 1/ASP Web App 1/Home.aspx.cs
@@ -9,51 +9,55 @@ namespace ASP_Web_App_1
 {
     public partial class Home : System.Web.UI.Page
     {
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            int age = DateTime.Now.Year - int.Parse(txtb_year.Text);
-            // save cookie .this
-            WriteCookie(age);
-            if(ReadCookie() > 15)
-            {
-                Response.Write("<script>alert('Successfully!!!');</script>");
-                Response.Redirect("Member.aspx");
-                
-            }
-            else
-            {
-                Response.Write("<script>alert('อายุน้อยกว่า 15ปี ห้ามเข้า!!!');</script>");
-            }
-
-        }
-        private void WriteCookie(int age)
-        {   // Create obj Cookie  Create parameter Cookie = UserSetting
-            HttpCookie userCookie = new HttpCookie("UserSetting");
-            // เก็บ ปีเกิดของ User
-            userCookie.Value = age.ToString();
-            // กำหนดวันหมดอายุของ Cookie
-            userCookie.Expires = DateTime.Now.AddDays(3d);
-            Response.Cookies.Add(userCookie);
-        }
-        private int ReadCookie()
-        {   // Create objec HTTP / parameter user old data = use old Data (Request.Cookies)
-            HttpCookie userCookie = Request.Cookies["UserSetting"];
-            return int.Parse(userCookie.Value);
-        }
-
         // Cookie จำเมื่อ โหลดจาก การเข้าใช้งานคราวที่แล้ว
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (ReadCookie() > 15)
+            // CheckAge(int.Parse(Request.Cookies["UserSetting"].Value));
+            // ReadSession();
+
+        }
+        private void CheckAge(int age)
+        {
+            if (age > 15)
             {
                 Response.Write("<script>alert('Successfully!!!');</script>");
                 Response.Redirect("Member.aspx");
-
             }
             else
             {
                 Response.Write("<script>alert('อายุน้อยกว่า 15ปี ห้ามเข้า!!!');</script>");
             }
         }
+        private void WriteSession(int age)
+        {
+            Session["userAge"] = age;
+        }
+        private int ReadSession()
+        {   if(Session["userAge"] == null)
+            {
+                return 0;
+            }
+            return int.Parse(Session["userAge"].ToString());
+        }
+      
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int age = DateTime.Now.Year - int.Parse(txtb_year.Text);
+            // WriteCookie(age);// save cookie .this
+            WriteSession(age);
+            CheckAge(int.Parse(Request.Cookies["UserSetting"].Value));
+        }
+        private void WriteCookie(int age)
+        {   HttpCookie userCookie = new HttpCookie("UserSetting");// Create obj Cookie  Create parameter Cookie = UserSetting
+            userCookie.Value = age.ToString();  // เก็บ ปีเกิดของ User
+            userCookie.Expires = DateTime.Now.AddDays(3d);            // กำหนดวันหมดอายุของ Cookie = 3 วัน
+            Response.Cookies.Add(userCookie);
+        }
+        private int ReadCookie()
+        {  HttpCookie userCookie = Request.Cookies["UserSetting"]; // Create objec HTTP / parameter user old data = use old Data (Request.Cookies)
+            return int.Parse(userCookie.Value);
+        }
+
+        
     }
 }
